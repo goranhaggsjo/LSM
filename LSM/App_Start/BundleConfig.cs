414d264c66bf9a16b40e7d33dc14bf4920d58b0d
Niclas Evaldssson <evald82@hotmail.com>
2018-03-09 08:59:15 +0100

    Börjat uppdatera edit

--- LSM/App_Start/BundleConfig.cs
+++ LSM/App_Start/BundleConfig.cs
@@ -10,9 +10,12 @@ namespace LSM
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
-
+          
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/courseinfo").Include(
+                        "~/Scripts/ScriptforLSM.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
---
+++ LSM/Scripts/ScriptforLSM.js
@@ -0,0 +1,20 @@
+﻿
+    $(document).ready(function () {
+        
+   
+
+
+        alert("Test")
+        
+
+        $("#button").on("click", function () {
+            alert("hej")
+            $("hej").html('@{ Html.RenderPartial("ViewPage1");}');
+            
+           
+
+
+        });
+
+
+    });
