fd4d39c5e367f9d3de041ac1a9c7694c2472e3d2
Niclas Evaldssson <evald82@hotmail.com>
2018-03-12 16:29:32 +0100

    Changes viewmodels

--- LSM/Migrations/201803121354566_Init.Designer.cs
+++ LSM/Migrations/201803121354566_Init.Designer.cs
@@ -7,13 +7,13 @@ namespace LSM.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class init : IMigrationMetadata
+    public sealed partial class Init : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(init));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(Init));
         
         string IMigrationMetadata.Id
         {
-            get { return "201803090923097_init"; }
+            get { return "201803121354566_Init"; }
         }
         
         string IMigrationMetadata.Source
