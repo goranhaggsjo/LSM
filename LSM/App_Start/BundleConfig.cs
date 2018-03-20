38138c3415f94170d3c4f8ffb8bfc1c8e38d39ab
Göran Häggsjö <Gran Hggsj@icloud.com>
2018-03-20 16:30:26 +0100

    commit for date-picker

--- LSM/App_Start/BundleConfig.cs
+++ LSM/App_Start/BundleConfig.cs
@@ -21,7 +21,8 @@ namespace LSM
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/scripts/bootstrap-datepicker.js",
+                      "~/Scripts/bootstrap-datepicker.sv.min.js",
+                      "~/Scripts/bootstrap-datepicker.min.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
